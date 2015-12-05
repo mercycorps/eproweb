640f7f4e7bd2d3a335ca47a7e37b93e16e1bf93c
Mahmood Khan <mkhan@mercycorps.org>
2015-12-04 17:09:48 -0800

    made some more work to make finance codes adding/edit work

--- htdocs/epro/forms.py
+++ htdocs/epro/forms.py
@@ -88,7 +88,13 @@ class FinanceCodesForm(forms.ModelForm):
         super(FinanceCodesForm, self).__init__(*args, **kwargs)
         form_action = kwargs['initial'].pop('form_action')
         self.helper = setup_boostrap_helpers(formtag=True)
-        self.helper.form_action = reverse_lazy(form_action if form_action else 'financecodes_new')
+        try:
+            item_id = kwargs['initial']['item_id']
+            self.helper.form_action = reverse_lazy(form_action, kwargs={'item_id': item_id})
+        except Exception as e:
+            pass
+        #self.helper.form_action = reverse_lazy(form_action if form_action else 'financecodes_new')
+
         self.helper.form_id = 'id_finance_codes_form'
         self.helper.label_class = 'col-sm-offset-0 col-sm-5'
         self.helper.field_class = 'col-sm-7'
--- htdocs/epro/urls.py
+++ htdocs/epro/urls.py
@@ -11,12 +11,19 @@ from epro.views import *
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="epro/home.html"), name='eprohome'),
     url(r'^feedback/$', FeedbackCreateView.as_view(), name='feedback'),
+
     url(r'^newpr/$', PurchaseRequestCreateView.as_view(), name='newpr'),
     url(r'^pr/(?P<pk>\d+)/$', PurchaseRequestDetailView.as_view(), name='pr_detail'),
+
     url(r'^item/add/(?P<pr>\d+)/$', PurchaseRequestItemCreateView.as_view(), name='item_new'),
     url(r'^item/edit/(?P<pk>\d+)/$', PurchaseRequestItemUpdateView.as_view(), name='item_edit'),
-    url(r'^financecodes_new/$', FinanceCodesCreateView.as_view(), name='financecodes_new'),
+    #url(r'^item/view/(?P<pk>\d+)/$', PurchaseRequestItemDetailView.as_view(), name='item_view'),
+    #url(r'^item/del/(?P<pk>\d+)/$', PurchaseRequestItemDeleteView.as_view(), name='item_del'),
 
+    url(r'^financecodes/add/(?P<item_id>\d+)/$', FinanceCodesCreateView.as_view(), name='financecodes_new'),
+    url(r'^financecodes/edit/(?P<pk>\d+)/$', FinanceCodesUpdateView.as_view(), name='financecodes_edit'),
+    #url(r'^financecodes/view/$', FinanceCodesDetailView.as_view(), name='financecodes_view'),
+    #url(r'^financecodes/del/$', FinanceCodesDeleteView.as_view(), name='financecodes_del'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
